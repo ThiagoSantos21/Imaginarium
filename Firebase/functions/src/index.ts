b0ec040ca161d2fc660f01e30a503267584ccf3e
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2022-03-23 17:20:53 -0300

    Modificações Firebase
    Criação do protótipo da função de adicionar um veículo ao BD

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -1,9 +1,27 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const app = admin.initializeApp();
+const db = app.firestore();
+const ticket = db.collection("ticket");
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const addTicket = functions
+    .region("southamerica-east1")
+    .https.onRequest(async (request, response) => {
+      const t = {
+        placa: String,
+        horaEntrada: Date,
+        horaSaida: Date,
+        tipoVeiculo: String,
+      };
+      try {
+        const docRef = await ticket.add(t);
+        response.send("veiculo inserido com sucesso.  " + docRef.id);
+      } catch (e) {
+        response.send("erro ao inserir veiculo");
+      }
+    });
