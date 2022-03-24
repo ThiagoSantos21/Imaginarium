8f385259c85de3ab444d050941115c446c8ec632
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2022-03-24 08:35:36 -0300

    Mudança das cores e criação da activity do itinerario

--- Android-Fiscal/app/src/main/java/com/example/imaginarium/MainActivity.kt
+++ Android-Fiscal/app/src/main/java/com/example/imaginarium/MainActivity.kt
@@ -8,20 +8,31 @@ import androidx.appcompat.widget.AppCompatButton
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btConsultar: AppCompatButton
+    private lateinit var btItinerario: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btConsultar = findViewById(R.id.btConsultar)
+        btItinerario = findViewById(R.id.btItinerario)
         btConsultar.setOnClickListener(){
+            val view = "ConsultActivity"
             startActivity()
+        }
+        btItinerario.setOnClickListener(){
+            startActivity2()
         }
 
     }
 
     private fun startActivity() {
         val intent = Intent(this, ConsultActivity::class.java)
+        startActivity(intent)
+
+    }
+    private fun startActivity2() {
+        val intent = Intent(this, ItineraryActivity::class.java)
         startActivity(intent)
 
     }
