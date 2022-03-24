ac3745744c50feaaaedf99704f4246139b5a3684
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2022-03-24 08:04:12 -0300

    Criação da função de busca

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -13,10 +13,10 @@ export const addTicket = functions
     .region("southamerica-east1")
     .https.onRequest(async (request, response) => {
       const t = {
-        placa: String,
-        horaEntrada: Date,
-        horaSaida: Date,
-        tipoVeiculo: String,
+        placa: "ABD1234", // String,
+        horaEntrada: "11:30:00", // Date,
+        horaSaida: "13:30:00", // Date
+        tipoVeiculo: "Carro", // String,
       };
       try {
         const docRef = await ticket.add(t);
@@ -24,4 +24,16 @@ export const addTicket = functions
       } catch (e) {
         response.send("erro ao inserir veiculo");
       }
+    });
+
+export const searchTicket = functions
+    .region("southamerica-east1")
+    .https.onRequest(async (request, response) => {
+      const placa = "ABC1234";
+      const snapshot = await ticket.where("placa", "==", placa).get();
+      const search : FirebaseFirestore.DocumentData = [];
+      snapshot.forEach((doc) => {
+        search.push(doc.data());
+      });
+      response.status(200).json(search);
     });
